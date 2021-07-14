e53bf3a8185d33a83e075c72d63e8dc3efd58b1b
AnimatorJoe <josephjin7@gmail.com>
2021-07-14 09:26:17 -0500

    mirroring user input on backend

--- generate-route/app.js
+++ generate-route/app.js
@@ -7,27 +7,41 @@ let response;
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
  * @param {Object} event - API Gateway Lambda Proxy Input Format
  *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
  * @param {Object} context
  *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
+ *
  */
 exports.lambdaHandler = async (event, context) => {
-    try {
-        // const ret = await axios(url);
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world',
-                // location: ret.data.trim()
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+  try {
+    // extract start point, end point, and safety params
+    const query = JSON.parse(event.body);
+    var { start, end } = query.points;
+    const safetyParams = query.safetyParams;
 
-    return response
+    // solve for route
+    var waypoints = [];
+
+    // generate return reoute
+    const route = {
+      start,
+      waypoints: waypoints,
+      end
+    };
+
+    // generate response object
+    response = {
+      statusCode: 200,
+      body: {
+        route
+      }
+    };
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+
+  return response;
 };
