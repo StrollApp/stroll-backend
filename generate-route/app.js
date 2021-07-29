404d166a0cf63599e36f3115f8d19dccbdc6de1b
Richard Wu <richardhwu@berkeley.edu>
2021-07-28 23:54:56 -0700

    implement select best path

--- generate-route/app.js
+++ generate-route/app.js
@@ -4,6 +4,7 @@ let response;
 
 const findClosestEdgeToPoint = require("./helper/findClosestEdgeToPoint");
 const findPathBetweenNodes = require("./helper/findPathBetweenNodes");
+const edgeScore = require("./helper/edgeScore");
 
 // import graph data
 const edgeArray = require("./edges.json");
@@ -48,6 +49,57 @@ exports.lambdaHandler = async (event, context) => {
     }
     (and similar for edges)
     */
+    const nodes = graph.nodes.slice();
+
+    const startParams = findClosestEdgeToPoint(graph, start);
+    const endParams = findClosestEdgeToPoint(graph, end);
+    const startEdge = startParams.edge;
+    const startLocation = startParams.location;
+    const endEdge = endParams.edge;
+    const endLocation = endParams.location;
+    const startNode0 = nodes.findIndex(element => element.id === startEdge.start_id);
+    const startNode1 = nodes.findIndex(element => element.id === startEdge.end_id);
+    const endNode0 = nodes.findIndex(element => element.id === endEdge.start_id);
+    const endNode1 = nodes.findIndex(element => element.id === endEdge.end_id);
+
+    const startEdgeDist = edgeScore(startEdge, safetyParams);
+    const endEdgeDist = edgeScore(endEdge, safetyParams);
+
+    const result00 = findPathBetweenNodes(graph, startNode0, endNode0, safetyParams);
+    const result01 = findPathBetweenNodes(graph, startNode0, endNode1, safetyParams);
+    const result10 = findPathBetweenNodes(graph, startNode1, endNode0, safetyParams);
+    const result11 = findPathBetweenNodes(graph, startNode1, endNode1, safetyParams);
+    const dists = [
+      startEdgeDist * startLocation + result00.dist + endEdgeDist * endLocation, 
+      startEdgeDist * startLocation + result01.dist + endEdgeDist * (1 - endLocation), 
+      startEdgeDist * (1 - startLocation) + result10.dist + endEdgeDist * endLocation, 
+      startEdgeDist * (1 - startLocation) + result11.dist + endEdgeDist * (1 - endLocation)
+    ];
+    var index = 0;
+    var min = Number.MAX_VALUE;
+    for (var i = 0; i < 4; i++) {
+      if (dists[i] < min) {
+        min = dists[i];
+        index = i;
+      }
+    }
+
+    var path;
+    if (index === 0) {
+      path = result00.path;
+    }
+    else if (index === 1) {
+      path = result01.path;
+    }
+    else if (index === 2) {
+      path = result10.path;
+    }
+    else if (index === 3) {
+      path = result11.path;
+    }
+
+    // TODO: do shit with the path and the endpoints
+
 /* after findClosestEdgeToPoint is done use this:
     const startEdge = findClosestEdgeToPoint(graph, start); // this should return an index...
     const endEdge = findClosestEdgeToPoint(graph, end);
