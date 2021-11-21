0b0944f64ac4039bbf9180cccc4a0e55134eaaf0
AnimatorJoe <josephjin7@gmail.com>
2021-11-21 14:35:33 -0800

    adding endpoint for heatmap queries

---
+++ get-heatmap-data/app.js
@@ -0,0 +1,60 @@
+const crimePoints = require("./data/crimeDataPoints.json");
+let response;
+
+/**
+ *
+ * get-heatmap-data takes an event object from the query that asks for certain data points for
+ * heatmap generation (ex: for crime, streetlight, foot traffic data etc.), and returns an array
+ * containing heatmap points.
+ *
+ * @param {Object} event - contains query information
+ * The event body should contain an object of the shape {"type": String}, specifying the type of
+ * data being queried for heatmap generation. The string can take on "CRIME", "STREET_LIGHTS",
+ * or "FOOT_TRAFFIC", though for now, it will only work for "CRIME".
+ *
+ * @returns [Object] array - contains array of data points
+ * Each data point will have the shape {"Longitude": Number, "Latitude": Number} and optionally a
+ * Number "Weight" attribute.
+ *
+ */
+exports.lambdaHandler = async (event, context) => {
+  try {
+    // extract claims
+    const claims = event.requestContext.authorizer.jwt.claims;
+
+    // extract type of map queried
+    const query = JSON.parse(event.body);
+    const type = query.type;
+
+    // log query
+    console.log(
+      `\n[GET HEATMAP DATE QUERY]:\n` +
+        `FROM ${JSON.stringify({
+          id: claims.user_id,
+          email_verified: claims.email_verified
+        })}\n` +
+        `FOR ${event.body}`
+    );
+
+    // set output data
+    var dataArray = [];
+    switch (type) {
+      case "CRIME":
+        dataArray = crimePoints;
+        break;
+      default:
+        break;
+    }
+
+    // generate response object
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({ dataArray })
+    };
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+
+  return response;
+};
